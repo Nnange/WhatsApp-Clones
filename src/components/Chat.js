9ee43f6ee5291d4fa37261dd2df7826e780cb3a3
Awong Nnange <awong.nnange@yahoo.com>
2020-10-09 17:12:56 +0200

    Added the side bar section of the App

---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../CSS/Chat.css"
+
+const Chat = () => {
+    return (
+        <div className="chat">
+            <h1>Hey Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeblBEV7wqchTyAfO52BtSvDn5unAInIA",
+    authDomain: "whatsapp-clone-6f076.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-6f076.firebaseio.com",
+    projectId: "whatsapp-clone-6f076",
+    storageBucket: "whatsapp-clone-6f076.appspot.com",
+    messagingSenderId: "1027475739101",
+    appId: "1:1027475739101:web:4e944a79d49e6bcc06e634",
+    measurementId: "G-JN470NRWM6"
+  };
