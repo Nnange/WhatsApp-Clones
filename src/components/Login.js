d27536ee815dbd75b0cb6af746090ff9efcc0bcb
Awong Nnange <awong.nnange@yahoo.com>
2020-10-13 23:59:58 +0200

    working on Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,16 @@ const Login = () => {
     return (
         <div className="login">
             <h1> Google Auth Login</h1>
+            <div className="login__container">
+                <img src="https://lh3.googleusercontent.com/qYEXlhwYdCseeGq1WlXApSluO_LcHJ2dSxvqmAXckY_8IXE2JPBfHpKq_7laiQeYJPGH7g=s85" alt=""/>
+                <div className="login__text">
+                    <h1>Sign in to WhatsApp</h1>
+                </div>
+
+                <Button type="submit" onClick={signIn}>
+                    Sign In with Google
+                </Button>
+            </div>
         </div>
     )
 }
