85c47ba5650aded034a9a0ed10ecc0a838f17f0d
Awong Nnange <awong.nnange@yahoo.com>
2020-10-18 21:28:49 +0200

    Updates

---
+++ precache-manifest.391b6e16cca5e9b008dcfb88d2c7213d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065bcd9561994e59a7bd8e02efb37069",
+    "url": "/WhatsApp-Clones/index.html"
+  },
+  {
+    "revision": "28817f6d1483d691232b",
+    "url": "/WhatsApp-Clones/static/css/main.a150da1a.chunk.css"
+  },
+  {
+    "revision": "7e0192a02b073e5db00b",
+    "url": "/WhatsApp-Clones/static/js/2.9d34bf85.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/WhatsApp-Clones/static/js/2.9d34bf85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28817f6d1483d691232b",
+    "url": "/WhatsApp-Clones/static/js/main.0a2e45dd.chunk.js"
+  },
+  {
+    "revision": "ad24d0c1b0d62cc6ffa2",
+    "url": "/WhatsApp-Clones/static/js/runtime-main.a814cc55.js"
+  }
+]);
